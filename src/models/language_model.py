9bc346a21a7e72f74bc0f624d8d11989752b0bb3
Mixchelle <michelle.biologia@hotmail.com>
2023-08-20 18:13:30 -0300

    REquisito 01

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,11 +1,21 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
 # Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db.languages
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def to_dict(self):
+        return self.data.copy()
+
+    @classmethod
+    def list_dicts(cls):
+        data = cls.find()
+        return [item.to_dict() for item in data]
 
     # Req. 2
     def to_dict(self):
