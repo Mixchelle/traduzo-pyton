b290dbd77c4fe9c979242e8e858f0b295690afa9
Mixchelle <michelle.biologia@hotmail.com>
2023-08-20 19:37:59 -0300

    Requisito 7

--- src/controllers/history_controller.py
+++ src/controllers/history_controller.py
@@ -4,7 +4,7 @@ from models.history_model import HistoryModel
 history_controller = Blueprint("history", __name__)
 
 
-@history_controller.route("/history", methods=["GET"])
+@history_controller.route("/", methods=["GET"])
 def list_history():
     history_records = HistoryModel.list_as_json()
-    return jsonify(history_records), 200
+    return history_records
